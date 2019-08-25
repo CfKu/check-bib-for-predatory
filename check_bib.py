82d64641e840f7bc980990dccb775b50f93ca23b
Christof Küstner <mail.to.cfk@gmail.com>
2019-08-25 22:23:59 +0200

    Initial commit of code

---
+++ check_bib.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © by Christof Küstner
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE"
+# wrote this file. As long as you retain this notice
+# you can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy me a beer in return.
+# Christof Küstner
+# ----------------------------------------------------------------------------
+
+"""
+@author: CfK
+"""
+
+# Prepare for Python3 (if called in Python2)
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+
+# =============================================================================
+# IMPORT
+# =============================================================================
+import argparse
+import requests
+import re
+import csv
+import html
+import lxml
+import lxml.html
+import unicodedata
+import os
+from tqdm import tqdm
+from pybtex.database.input import bibtex
+import colorama
+
+# LOCAL IMPORT
+from check_bib import print_title, print_colored_status, \
+    print_report, get_cache_csv, get_cosine_sim_score
+
+# =============================================================================
+# CONSTANTS USER
+# =============================================================================
+SIMILARITY_THRESHOLDS = (0.7, 0.75, 0.8)
+PREDATORY_SOURCES = {
+    # {URL: (LXML-ETREE-ELEMENTS, ELEMENT-REGEX, [BIB-FIELDS,]}
+    r"https://predatoryjournals.com/journals/":
+        (
+            r"//li[not(@id) and not(@class)]",
+            r"./a/@href",
+            ["journal", "journaltitle", "booktitle"]
+        ),
+    r"https://beallslist.weebly.com/":
+        (
+            r"//li[not(@id) and not(@class)]",
+            r"./a/@href",
+            ["publisher"]
+        ),
+    r"https://beallslist.weebly.com/standalone-journals.html":
+        (
+            r"//li[not(@id) and not(@class)]",
+            r"./a/@href",
+            ["journal", "journaltitle", "booktitle"]
+        ),
+}
+# =============================================================================
+# CONSTANTS APP
+# =============================================================================
+RE_LATEX_COMMANDS_STRIP = re.compile(r"\\[^\s]*{(.*?)}")
+RE_HTML_COMMANDS_STRIP = re.compile(r"<[^>]+>")
+
+
+# =============================================================================
+# DEFINITION
+# =============================================================================
+def crawl_predatory_sources():
+    print_colored_status(
+        "Crawl and cache predatory journals and publishers from given URLs", 1)
+    for source_url, (source_pj_element, source_pj_url, _) in PREDATORY_SOURCES.items():
+        cache_csv_filename = get_cache_csv(source_url)
+        with open(cache_csv_filename, mode="w") as cache_file:
+            # prepare cache CSV
+            cache_writer = csv.writer(
+                cache_file, delimiter=",", quotechar='"',
+                quoting=csv.QUOTE_MINIMAL)
+            # get html code of source url
+            r = requests.get(source_url)
+            if r.status_code == 200:  # succeeded
+                pj_counter = 0
+                tqdm_desc = "  + '{}'".format(source_url)
+                # crawl predatory journals names, urls, and short names
+                html_tree = lxml.html.fromstring(r.content)
+                pj_elements = html_tree.xpath(source_pj_element)
+                for pj_element in tqdm(pj_elements, desc=tqdm_desc, unit=""):
+                    # element_text = lxml.etree.tostring(
+                    #    element).decode().strip()
+                    pj_urls = pj_element.xpath(source_pj_url)
+                    if pj_urls and pj_urls[0].startswith("http"):
+                        pj_name = unicodedata.normalize(
+                            "NFKD", pj_element.text_content())
+                        pj_url = pj_urls[0]
+                        cache_row = [pj_name, pj_url]
+                        # add pj to csv
+                        pj_counter += 1
+                        cache_writer.writerow(cache_row)
+
+                print("  => Crawled {} journals/"
+                      "publishers.".format(pj_counter))
+    print()
+
+
+def check_bibliography(bib_file):
+    print_colored_status(
+        "Read predatory journals and publishers from local CSV cache", 1)
+    # 1: create index of predatory journals / publishers
+    idx_pj = {}
+    for source_url, (_, _, compare_fields) in PREDATORY_SOURCES.items():
+        cache_csv_filename = get_cache_csv(source_url)
+
+        with open(cache_csv_filename, mode="r") as cache_file:
+            cache_reader = csv.reader(cache_file,
+                                      delimiter=",", quotechar='"')
+            # create indexes including compare_fields (names)
+            # to be double-checked
+            for row in cache_reader:
+                pj_name, pj_url = row
+                idx_pj[pj_name.lower()] = [pj_name, pj_url, compare_fields]
+    print("   > Added {} unique predatory journals/publishers "
+          "to index.".format(len(idx_pj)))
+    print()
+
+    print_colored_status("Compare predatory index with bib-file entries", 1)
+    # 2: compare index items with bib file
+    parser = bibtex.Parser()
+    bib = parser.parse_file(bib_file)
+    # iterate over every bib item in bib_file
+    report = {}
+    for bib_key in tqdm(bib.entries.keys(), unit="keys",
+                        desc="Double-check bib keys"):
+        # get all filed names of bib_key entry
+        bib_fields = bib.entries[bib_key].fields.keys()
+        # compare required fields with predatory journals/publishers in index
+        for _, (pj_name, pj_url, compare_fields) in idx_pj.items():
+            for bib_field in bib_fields:
+                # check if field needs to be double-checked
+                if bib_field.lower() not in compare_fields:
+                    continue
+
+                # get value/content of field to be double-checked
+                bib_entry = bib.entries[bib_key].fields[bib_field]
+                # strip latex special characters
+                bib_entry = RE_LATEX_COMMANDS_STRIP.sub(r"\1", bib_entry)
+                bib_entry = bib_entry.replace(r"\&", r"&")
+
+                # compute score
+                similarity_score = get_cosine_sim_score(
+                    bib_entry.lower(), pj_name.lower())[0][1]
+
+                # add to report if score is over smallest given threshold
+                thr0 = SIMILARITY_THRESHOLDS[0]
+                if (thr0 <= similarity_score):
+                    # prepare report dict (if required)
+                    if bib_key not in report:
+                        report[bib_key] = {}
+                    if bib_field not in report[bib_key]:
+                        report[bib_key][bib_field] = (bib_entry, [])
+                    # add new match
+                    report[bib_key][bib_field][1].append([
+                        similarity_score,
+                        pj_name,
+                        pj_url
+                    ])
+
+    # print report
+    print()
+    print_colored_status("Report", 1)
+    print_report(report, SIMILARITY_THRESHOLDS)
+
+
+def main():
+    # ---------------------------------------------------------- PRE PROCESSING
+    # define cli options
+    cli_parser = argparse.ArgumentParser()
+    cli_parser.add_argument("bib_file", type=argparse.FileType("r"),
+                            help=("BibTeX file to be double-checked"))
+    cli_parser.add_argument("--refresh", action="store_true", default=False,
+                            help=("Refresh the local predatory CSV cache"))
+    # parse cli options
+    cli_args = cli_parser.parse_args()
+
+    # -------------------------------------------------------------- PROCESSING
+    print_title("Double-check bibliography (BibTeX, bib) for predatory "
+                "publishers and journals", "CfK", "~", 120)
+    print()
+
+    # crawl given sources (if required)
+    if cli_args.refresh:
+        crawl_predatory_sources()
+    # check bibliography
+    check_bibliography(cli_args.bib_file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ check_bib_helpers.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © by Christof Küstner
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE"
+# wrote this file. As long as you retain this notice
+# you can do whatever you want with this stuff. If we meet some day, and you
+# think this stuff is worth it, you can buy me a beer in return.
+# Christof Küstner
+# ----------------------------------------------------------------------------
+
+"""
+@author: CfK
+"""
+
+# =============================================================================
+# IMPORT
+# =============================================================================
+import platform
+import colorama
+import re
+import urllib.parse
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from collections import Counter
+
+
+# =============================================================================
+# CONST
+# =============================================================================
+IS_WINDOWS = platform.system() == "Windows"
+IS_MACOS = platform.system() == "Darwin"
+IS_LINUX = platform.system() == "Linux"
+
+if IS_WINDOWS:
+    colorama.init()
+
+
+# =============================================================================
+# DEFINITION
+# =============================================================================
+def print_title(app_title, app_author, filler, terminal_size):
+    title = " "
+    title += colorama.Style.BRIGHT + app_title + colorama.Style.RESET_ALL
+    title += " by "
+    title += colorama.Style.BRIGHT + app_author + colorama.Style.RESET_ALL
+    title += " " + 3*filler
+    print(title.rjust(terminal_size, filler) + colorama.Style.RESET_ALL)
+    print()
+
+
+def print_colored_status(text, status_color_id=0):
+    # status to color store
+    COLORS = (colorama.Fore.RED + colorama.Style.BRIGHT,
+              colorama.Fore.GREEN + colorama.Style.BRIGHT,
+              colorama.Fore.YELLOW + colorama.Style.BRIGHT)
+
+    # print colored status
+    if status_color_id < len(COLORS):
+        if status_color_id == 0:
+            print(COLORS[status_color_id] + ">>> " +
+                  text + colorama.Style.RESET_ALL)
+        else:
+            print(COLORS[status_color_id] + ">>> " +
+                  colorama.Style.RESET_ALL + text)
+    else:
+        print(text)
+
+
+def get_cache_csv(url):
+    return r"predatory_cache_{}.csv".format(urllib.parse.quote(url, safe=""))
+
+def highlight_similarity(hl_words, sentence):
+    re_from = r"(" + r"|".join(hl_words.replace(r"(", r"\(").
+                               replace(r")", r"\)").split(" ")) + r")"
+    re_to = colorama.Fore.LIGHTCYAN_EX + r"\1" + colorama.Style.RESET_ALL
+    return re.sub(re_from, re_to, sentence)
+
+
+def get_jaccard_sim_score(str1, str2):
+    a = set(str1.split())
+    b = set(str2.split())
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
+
+
+def get_vectors(*strs):
+    text = [t for t in strs]
+    vectorizer = CountVectorizer(text)
+    vectorizer.fit(text)
+    return vectorizer.transform(text).toarray()
+
+
+def get_cosine_sim_score(*strs):
+    vectors = [t for t in get_vectors(*strs)]
+    return cosine_similarity(vectors)
+
+
+def score_color(score, thresholds):
+    thr0, thr1, thr2 = thresholds
+    if thr0 <= score < thr1:
+        return colorama.Fore.YELLOW
+    elif thr1 <= score < thr2:
+        return colorama.Fore.LIGHTRED_EX + \
+            colorama.Style.BRIGHT
+    else:
+        return colorama.Back.RED + colorama.Fore.BLACK
+
+
+def print_report(report, thresholds):
+    for bib_key, bib_matches in report.items():
+        # report bib_key
+        print("Similarites found in '{}{}{}':".format(
+            colorama.Back.WHITE + colorama.Fore.BLACK,
+            bib_key,
+            colorama.Style.RESET_ALL
+        ))
+        for bib_field, (bib_entry, pj_matches) in bib_matches.items():
+            # report bib_field
+            print("   {} : {}{}{}".format(
+                bib_field.rjust(15, " "),
+                colorama.Fore.WHITE + colorama.Style.BRIGHT,
+                bib_entry,
+                colorama.Style.RESET_ALL
+            ))
+            # print match
+            for similarity_score, pj_name, pj_url in \
+                    sorted(pj_matches, reverse=True):
+                print("   {}{}{:.2f}{} | {} >> URL:{}".format(
+                    " " * (15 - 4),
+                    score_color(similarity_score, thresholds),
+                    similarity_score,
+                    colorama.Style.RESET_ALL,
+                    highlight_similarity(
+                        bib_entry, pj_name.ljust(100, " ")),
+                    pj_url
+                ))
