bb28caf11e37a34eb9249fb677e6717ab16a5cf3
Christopher Sauer <christopher.sauer@me.com>
2025-02-15 19:14:54 +0100

    feat(check_bib): add parallelization and ruffed file

--- check_bib.py
+++ check_bib.py
@@ -16,8 +16,7 @@
 """
 
 # Prepare for Python3 (if called in Python2)
-from __future__ import (
-    absolute_import, division, print_function, unicode_literals)
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 
 # =============================================================================
@@ -32,10 +31,17 @@ import lxml.html
 import unicodedata
 from tqdm import tqdm
 from pybtex.database.input import bibtex
+import multiprocessing
+from functools import partial
 
 # LOCAL IMPORT
-from check_bib_helpers import print_title, print_colored_status, \
-    print_report, get_cache_csv, get_cosine_sim_score
+from check_bib_helpers import (
+    print_title,
+    print_colored_status,
+    print_report,
+    get_cache_csv,
+    get_cosine_sim_score,
+)
 
 # =============================================================================
 # CONSTANTS USER
@@ -44,44 +50,38 @@ SIMILARITY_THRESHOLDS = (0.7, 0.75, 0.8)
 PREDATORY_SOURCES = {
     # {URL: (LXML-ETREE-ELEMENTS, ELEMENT-REGEX, [BIB-FIELDS,]}
     # https://raw.github.com/stop-predatory-journals/stop-predatory-journals.github.io/master/_data/journals.csv
-    r"https://predatoryjournals.com/journals/":
-        (
-            r"//li[not(@id) and not(@class)]",
-            r"./a/@href",
-            ["journal", "journaltitle", "booktitle"]
-        ),
+    r"https://predatoryjournals.com/journals/": (
+        r"//li[not(@id) and not(@class)]",
+        r"./a/@href",
+        ["journal", "journaltitle", "booktitle"],
+    ),
     # https://raw.github.com/stop-predatory-journals/stop-predatory-journals.github.io/master/_data/hijacked.csv
-    r"https://predatoryjournals.com/hijacked/":
-        (
-            r"//td[not(@id) and not(@class)]",
-            r"./a/@href",
-            ["journal", "journaltitle", "booktitle"]
-        ),
+    r"https://predatoryjournals.com/hijacked/": (
+        r"//td[not(@id) and not(@class)]",
+        r"./a/@href",
+        ["journal", "journaltitle", "booktitle"],
+    ),
     # https://raw.github.com/stop-predatory-journals/stop-predatory-journals.github.io/master/_data/publishers.csv
-    r"https://predatoryjournals.com/publishers/":
-        (
-            r"//li[not(@id) and not(@class)]",
-            r"./a/@href",
-            ["publisher"]
-        ),
-    r"https://beallslist.net/":
-        (
-            r"//li[not(@id) and not(@class)]",
-            r"./a/@href",
-            ["publisher"]
-        ),
-    r"https://beallslist.net/standalone-journals/":
-        (
-            r"//li[not(@id) and not(@class)]",
-            r"./a/@href",
-            ["journal", "journaltitle", "booktitle"]
-        ),
-    r"https://beallslist.net/hijacked-journals/":
-        (
-            r"//td[not(@id) and not(@class)]",
-            r"./a/@href",
-            ["journal", "journaltitle", "booktitle"]
-        ),
+    r"https://predatoryjournals.com/publishers/": (
+        r"//li[not(@id) and not(@class)]",
+        r"./a/@href",
+        ["publisher"],
+    ),
+    r"https://beallslist.net/": (
+        r"//li[not(@id) and not(@class)]",
+        r"./a/@href",
+        ["publisher"],
+    ),
+    r"https://beallslist.net/standalone-journals/": (
+        r"//li[not(@id) and not(@class)]",
+        r"./a/@href",
+        ["journal", "journaltitle", "booktitle"],
+    ),
+    r"https://beallslist.net/hijacked-journals/": (
+        r"//td[not(@id) and not(@class)]",
+        r"./a/@href",
+        ["journal", "journaltitle", "booktitle"],
+    ),
 }
 # =============================================================================
 # CONSTANTS APP
@@ -95,14 +95,17 @@ RE_HTML_COMMANDS_STRIP = re.compile(r"<[^>]+>")
 # =============================================================================
 def crawl_predatory_sources():
     print_colored_status(
-        "Crawl and cache predatory journals and publishers from given URLs", 1)
+        "Crawl and cache predatory journals and publishers from given URLs", 1
+    )
     for source_url, (source_pj_element, source_pj_url, _) in PREDATORY_SOURCES.items():
         cache_csv_filename = get_cache_csv(source_url)
-        with open(cache_csv_filename, mode="w", encoding="utf8", newline="") as cache_file:
+        with open(
+            cache_csv_filename, mode="w", encoding="utf8", newline=""
+        ) as cache_file:
             # prepare cache CSV
             cache_writer = csv.writer(
-                cache_file, delimiter=",", quotechar='"',
-                quoting=csv.QUOTE_MINIMAL)
+                cache_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+            )
             # get html code of source url
             r = requests.get(source_url)
             if r.status_code == 200:  # succeeded
@@ -117,93 +120,130 @@ def crawl_predatory_sources():
                     pj_urls = pj_element.xpath(source_pj_url)
                     if pj_urls and pj_urls[0].startswith("http"):
                         pj_name = unicodedata.normalize(
-                            "NFKD", pj_element.text_content())
+                            "NFKD", pj_element.text_content()
+                        )
                         pj_url = pj_urls[0]
                         cache_row = [pj_name, pj_url]
                         # add pj to csv
                         pj_counter += 1
                         cache_writer.writerow(cache_row)
 
-                print("  => Crawled {} journals/"
-                      "publishers.".format(pj_counter))
+                print("  => Crawled {} journals/publishers.".format(pj_counter))
     print()
 
 
+def process_bib_entry(idx_pj, bib_entries, bib_key):
+    """Process a single bibliography entry"""
+    report_entry = {}
+    # get all field names of bib_key entry
+    bib_fields = bib_entries[bib_key].fields.keys()
+
+    # compare required fields with predatory journals/publishers in index
+    for _, (pj_name, pj_url, compare_fields) in idx_pj.items():
+        for bib_field in bib_fields:
+            # check if field needs to be double-checked
+            if bib_field.lower() not in compare_fields:
+                continue
+
+            # get value/content of field to be double-checked
+            bib_entry = bib_entries[bib_key].fields[bib_field]
+            # strip latex special characters
+            bib_entry = RE_LATEX_COMMANDS_STRIP.sub(r"\1", bib_entry)
+            bib_entry = bib_entry.replace(r"\&", r"&")
+
+            # compute score
+            similarity_score = get_cosine_sim_score(bib_entry.lower(), pj_name.lower())[
+                0
+            ][1]
+
+            # add to report if score is over smallest given threshold
+            thr0 = SIMILARITY_THRESHOLDS[0]
+            if thr0 <= similarity_score:
+                # prepare report dict (if required)
+                if bib_field not in report_entry:
+                    report_entry[bib_field] = (bib_entry, [])
+                # add new match
+                report_entry[bib_field][1].append([similarity_score, pj_name, pj_url])
+
+    return bib_key, report_entry if report_entry else None
+
+
 @click.command()
-@click.argument("bib_file", type=click.File("r", encoding="utf8"),
-                required=True)
-@click.option("--refresh", "refresh_index", default=False, is_flag=True,
-              help="Refresh the local predatory CSV cache")
+@click.argument("bib_file", type=click.File("r", encoding="utf8"), required=True)
+@click.option(
+    "--refresh",
+    "refresh_index",
+    default=False,
+    is_flag=True,
+    help="Refresh the local predatory CSV cache",
+)
 def check_bibliography(bib_file, refresh_index=False):
     """
     Double-check bibliography (BibTeX, bib => BIB_FILE) for predatory publishers and journals
     """
-    print_title("Double-check bibliography (BibTeX, bib) for predatory "
-                "publishers and journals", "CfK", "~", 120)
+    print_title(
+        "Double-check bibliography (BibTeX, bib) for predatory publishers and journals",
+        "CfK",
+        "~",
+        120,
+    )
     print()
     if refresh_index:
         crawl_predatory_sources()
     # -------------------------------------------------------------- PROCESSING
     print_colored_status(
-        "Read predatory journals and publishers from local CSV cache", 1)
+        "Read predatory journals and publishers from local CSV cache", 1
+    )
     # 1: create index of predatory journals / publishers
     idx_pj = {}
     for source_url, (_, _, compare_fields) in PREDATORY_SOURCES.items():
         cache_csv_filename = get_cache_csv(source_url)
 
         with open(cache_csv_filename, mode="r", encoding="utf8") as cache_file:
-            cache_reader = csv.reader(cache_file,
-                                      delimiter=",", quotechar='"')
+            cache_reader = csv.reader(cache_file, delimiter=",", quotechar='"')
             # create indexes including compare_fields (names)
             # to be double-checked
             for row in cache_reader:
                 pj_name, pj_url = row
                 idx_pj[pj_name.lower()] = [pj_name, pj_url, compare_fields]
-    print("   > Added {} unique predatory journals/publishers "
-          "to index.".format(len(idx_pj)))
+    print(
+        "   > Added {} unique predatory journals/publishers to index.".format(
+            len(idx_pj)
+        )
+    )
     print()
 
     print_colored_status("Compare predatory index with bib-file entries", 1)
     # 2: compare index items with bib file
     parser = bibtex.Parser()
     bib = parser.parse_file(bib_file)
-    # iterate over every bib item in bib_file
+
+    # Prepare for parallel processing
+    num_cores = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=num_cores)
+
+    # Create partial function with fixed arguments
+    process_entry = partial(process_bib_entry, idx_pj, bib.entries)
+
+    # Process entries in parallel with progress bar
+    results = list(
+        tqdm(
+            pool.imap(process_entry, bib.entries.keys()),
+            total=len(bib.entries),
+            unit="keys",
+            desc=f"Double-check bib keys (using {num_cores} cores)",
+        )
+    )
+
+    # Close the pool
+    pool.close()
+    pool.join()
+
+    # Combine results into report
     report = {}
-    for bib_key in tqdm(bib.entries.keys(), unit="keys",
-                        desc="Double-check bib keys"):
-        # get all filed names of bib_key entry
-        bib_fields = bib.entries[bib_key].fields.keys()
-        # compare required fields with predatory journals/publishers in index
-        for _, (pj_name, pj_url, compare_fields) in idx_pj.items():
-            for bib_field in bib_fields:
-                # check if field needs to be double-checked
-                if bib_field.lower() not in compare_fields:
-                    continue
-
-                # get value/content of field to be double-checked
-                bib_entry = bib.entries[bib_key].fields[bib_field]
-                # strip latex special characters
-                bib_entry = RE_LATEX_COMMANDS_STRIP.sub(r"\1", bib_entry)
-                bib_entry = bib_entry.replace(r"\&", r"&")
-
-                # compute score
-                similarity_score = get_cosine_sim_score(
-                    bib_entry.lower(), pj_name.lower())[0][1]
-
-                # add to report if score is over smallest given threshold
-                thr0 = SIMILARITY_THRESHOLDS[0]
-                if (thr0 <= similarity_score):
-                    # prepare report dict (if required)
-                    if bib_key not in report:
-                        report[bib_key] = {}
-                    if bib_field not in report[bib_key]:
-                        report[bib_key][bib_field] = (bib_entry, [])
-                    # add new match
-                    report[bib_key][bib_field][1].append([
-                        similarity_score,
-                        pj_name,
-                        pj_url
-                    ])
+    for bib_key, entry_report in results:
+        if entry_report:  # Only add entries that have matches
+            report[bib_key] = entry_report
 
     # print report
     print()
@@ -211,5 +251,5 @@ def check_bibliography(bib_file, refresh_index=False):
     print_report(report, SIMILARITY_THRESHOLDS)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_bibliography()
